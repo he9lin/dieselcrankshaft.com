e99ef93df5254779790a793a6935b0f0b0813b50
Lin He <he9lin@gmail.com>
2011-05-19 19:51:01 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cranker::Application.config.secret_token = '36c9f3d9cc53df707e600b812dde971702b450c3578bb89126cef436ac2fa050fb2066a9ac1e757a4a4fda4c577ed7e1807173aee305c376ea260cc08c100a5b'
