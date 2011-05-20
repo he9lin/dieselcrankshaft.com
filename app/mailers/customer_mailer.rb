8af40b92991517ae3d4810f0473565b3c7c03aed
Lin He <he9lin@gmail.com>
2011-05-20 11:03:53 -0700

    cranker

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,9 +1,9 @@
 class CustomerMailer < ActionMailer::Base
-  default :from => "crank518@gmail.com"  
+  default :from => "cranker518@gmail.com"  
 
   def inquery(params)  
     @params = params
-    mail(:to => "crank518@gmail.com", :subject => "dieselcrankshaft.com inquery: #{params[:subject]}")  
+    mail(:to => "cranker518@gmail.com", :subject => "dieselcrankshaft.com inquery: #{params[:subject]}")  
   end
   
 end
